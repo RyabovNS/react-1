6ccf66344eda79caaf1d2a3565c6b380b840d913
Nikita Ryabov <nikita.kimovsk@mail.ru>
2022-05-27 14:59:38 +0300

    Container component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import Post from './MyPost/Post';
 import s from './MyPosts.module.css';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
-
-
 
 const MyPosts = (props) => {
-
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
-
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.AddPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return (
@@ -32,7 +26,7 @@ const MyPosts = (props) => {
                                   value={props.newPostText}/>
                     </div>
                     <div>
-                        <button onClick={ addPost }>Add post</button>
+                        <button onClick={ onAddPost }>Add post</button>
                     </div>
                 </div>
                 <div className={s.posts}>
