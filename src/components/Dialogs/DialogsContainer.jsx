28151d63ce142e6907a6758e1098810515e38bfb
Nikita Ryabov <nikita.kimovsk@mail.ru>
2022-05-30 11:56:26 +0300

    react-redux

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,23 +1,36 @@
-import s from "./Dialogs.module.css";
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
+import {connect} from "react-redux";
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState().dialogsPage;
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    }
-
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
-
-    return (<Dialogs updateNewMessageBodyCreator={onNewMessageChange}
-                     sendMessage={onSendMessageClick}
-                     dialogsPage={state}/>);
+const DialogsContainer = () => {
+    return <StoreContext.Consumer>
+        { (store) => {
+            let onSendMessageClick = () => {
+                store.dispatch(sendMessageCreator());
+            }
+            let onNewMessageChange = (body) => {
+                store.dispatch(updateNewMessageBodyCreator(body));
+            }
+            return <Dialogs updateNewMessageBodyCreator={onNewMessageChange}
+                            sendMessage={onSendMessageClick}
+                            dialogsPage={store.getState().dialogsPage}/>
+            }
+        }
+    </StoreContext.Consumer>
 }
+
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+let f2 = () => {
+    return {
+
+    }
+}
+
+const SuperDialogsContainer = connect(mapStateToProps, f2)(Dialogs);
 
 export default DialogsContainer;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = (props) => {
     return (
         <div className={s}>
             <ProfileInfo/>
-            <MyPostsContainer store={props.store}/>
+            <MyPostsContainer/>
         </div>
     );
 }
