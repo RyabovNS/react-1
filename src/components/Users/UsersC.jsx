b6ec5d507818b73d37f9f6172297f51ac300571a
Nikita Ryabov <nikita.kimovsk@mail.ru>
2022-06-07 12:04:26 +0300

    Added a class component

---
+++ src/components/Users/UsersC.jsx
@@ -0,0 +1,9 @@
+import s from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.png"
+
+class User extends React.Component {
+
+}
+
+export default Users;
