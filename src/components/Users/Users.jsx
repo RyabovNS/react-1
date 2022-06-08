bdfd33c3b6634e5faebef21a2cdc41552084b46c
Nikita Ryabov <nikita.kimovsk@mail.ru>
2022-06-08 12:03:20 +0300

    Applied componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,19 @@
+import React from 'react';
 import s from "./Users.module.css";
 import axios from "axios";
-import userPhoto from "../../assets/images/user.png"
+import userPhoto from "../../assets/images/user.png";
 
-const Users = (props) => {
-
-    const getUsers = () => {
-        if ((props.users.length) === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
-            });
-        }
-
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
-
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                <button onClick={this.getUsers}>Get Users</button>
+                {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
@@ -24,14 +21,14 @@ const Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -41,9 +38,10 @@ const Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
-    );
+                </div>)}
+            </div>
+        );
+    }
 }
 
 export default Users;
--- src/components/Users/UsersC.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./Users.module.css";
-import axios from "axios";
-import userPhoto from "../../assets/images/user.png"
-
-class User extends React.Component {
-
-}
-
-export default Users;
