37c0c9a94d1f4729b30f19c3dc2fadc71f80c2af
Nikita Ryabov <nikita.kimovsk@mail.ru>
2022-06-17 17:09:20 +0300

    Authentication

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import s from './Header.module.css';
 
-const Header = () => {
-	return (
-		<header className={s.header}>
-			<img src='https://w7.pngwing.com/pngs/818/848/png-transparent-coca-cola-pepsi-globe-logo-pepsi-beverage-can-red-pepsi-thumbnail.png' />
-		</header>
-	);
+import {NavLink} from "react-router-dom";
+
+const Header = (props) => {
+    return (
+        <header className={s.header}>
+            <img
+                src='https://w7.pngwing.com/pngs/818/848/png-transparent-coca-cola-pepsi-globe-logo-pepsi-beverage-can-red-pepsi-thumbnail.png'/>
+            <div className={s.loginBlock}>
+                {props.isAuth
+                    ? props.login
+                    : <NavLink to={'login'}>Login</NavLink>}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
